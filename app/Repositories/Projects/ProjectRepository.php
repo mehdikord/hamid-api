d03b003166b9f8a92e123899784356c506f7ca4b
Mehdi Kord <mehdi.kord171@gmail.com>
2025-05-24 18:53:37 +0330

    updated

--- app/Repositories/Projects/ProjectRepository.php
+++ app/Repositories/Projects/ProjectRepository.php
@@ -172,10 +172,16 @@ class ProjectRepository implements ProjectInterface
         if (request()->filled('search') && request()->search['user_id'] != 0 ){
             $data->whereHas('user', function ($query) {$query->where('user_id', request()->search['user_id']);});
         }
-        if (request()->filled('search') && request()->search['phone'] ){
+        if (request()->filled('search') && isset(request()->search['phone']) && request()->search['phone'] ){
             $data->whereHas('customer', function ($query) {$query->where('phone','LIKE','%'.request()->search['phone'].'%');});
         }
-
+        if (request()->filled('search') && request()->search['has_name'] != 'all' ){
+            if (request()->search['has_name'] == '1'){
+                $data->whereHas('customer', function ($query) {$query->whereNotNull('name');});
+            }else{
+                $data->whereHas('customer', function ($query) {$query->whereNull('name');});
+            }
+        }
         if (request()->filled('search') && request()->search['has_report'] != 'all' ){
             if (request()->search['has_report'] == 1){
                 $data->whereHas('reports');
