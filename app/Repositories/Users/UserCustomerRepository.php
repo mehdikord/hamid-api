c04907383758f3b013d01a6b1db31c4d59e52ca7
Mehdi Kord <mehdi.kord171@gmail.com>
2025-09-20 18:31:13 +0330

    Enhance invoice storage process in UserCustomerRepository by removing unnecessary return statement and ensuring consistent logging for group invoice notifications.

--- app/Repositories/Users/UserCustomerRepository.php
+++ app/Repositories/Users/UserCustomerRepository.php
@@ -350,7 +350,6 @@ class UserCustomerRepository implements UserCustomerInterface
         }
         // activity log
         helper_activity_create(null,null,$customer->project_id,$customer->customer_id,'ثبت فاکتور',"# : ثبت فاکتور ".$item->id."");
-        return 'salam';
         helper_bot_send_group_invoice($item);
         return helper_response_fetch(new UserCustomerInvoiceResource($item));
     }
@@ -493,6 +492,7 @@ class UserCustomerRepository implements UserCustomerInterface
         ]);
         //activity log
         helper_activity_create(null,null,$project_customer->project_id,$project_customer->customer_id,'ثبت فاکتور',"# : ثبت فاکتور ".$item->id."");
+        helper_bot_send_group_invoice($item);
         return helper_response_fetch(new UserCustomerInvoiceResource($item));
     }
 
