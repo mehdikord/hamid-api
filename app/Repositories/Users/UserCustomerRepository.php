3cb23d13e5ed60c02e0a54e3c254f0354b33d292
Mehdi Kord <mehdi.kord171@gmail.com>
2025-09-20 18:34:19 +0330

    update

--- app/Repositories/Users/UserCustomerRepository.php
+++ app/Repositories/Users/UserCustomerRepository.php
@@ -492,6 +492,7 @@ class UserCustomerRepository implements UserCustomerInterface
         ]);
         //activity log
         helper_activity_create(null,null,$project_customer->project_id,$project_customer->customer_id,'ثبت فاکتور',"# : ثبت فاکتور ".$item->id."");
+        return 'hello';
         helper_bot_send_group_invoice($item);
         return helper_response_fetch(new UserCustomerInvoiceResource($item));
     }
