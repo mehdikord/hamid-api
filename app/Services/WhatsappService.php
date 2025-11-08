e17abaee77d541ede494eca770fa8c02735fbda9
Mehdi Kord <mehdi.kord171@gmail.com>
2025-11-08 15:12:45 +0330

    Add WhatsApp queue command and enhance WhatsAppService: Introduced a new console command for processing WhatsApp messages from the queue, and updated the WhatsAppService to support admin-specific message handling and logging, improving the overall functionality of WhatsApp message management.

--- app/Services/WhatsappService.php
+++ app/Services/WhatsappService.php
@@ -6,6 +6,7 @@ This service is used to send and receive whatsapp messages from and to customers
 
 namespace App\Services;
 
+use App\Models\Project;
 use App\Models\Whatsapp\WhatsappLog;
 use App\Models\Whatsapp\WhatsappNumber;
 use App\Models\Whatsapp\WhatsappQueue;
@@ -22,7 +23,17 @@ class WhatsappService
 
     public function send_message($message,$phone,$customer_id=null,$project_id=null)
     {
-        $number = $this->active_number();
+
+        $admin_id = null;
+        if(auth('admins')->check()){
+            $admin_id = auth('admins')->id();
+        }else{
+            $project = Project::find($project_id);
+            if($project){
+                $admin_id = $project->member_id;
+            }
+        }
+        $number = $this->active_number(admin_id: $admin_id);
         $data =[];
         $data['message'] = $message;
         $data['number'] = $phone;
@@ -34,14 +45,14 @@ class WhatsappService
                 'use_count' => $number->use_count + 1,
             ]);
             if($result['status']){
-                $this->log_create($number->id,$message,$phone,true,$customer_id,$project_id,$result['message']);
+                $this->log_create($number->id,$message,$phone,true,$customer_id,$project_id,$result['message'],$admin_id);
                 return 'success';
             }else{
-                $this->log_create($number->id,$message,$phone,false,$customer_id,$project_id,$result['message']);
+                $this->log_create($number->id,$message,$phone,false,$customer_id,$project_id,$result['message'],$admin_id);
                 return 'error';
             }
         }
-        $this->add_to_queue($message,$phone,$customer_id,$project_id);
+        $this->add_to_queue($message,$phone,$customer_id, $project_id,$admin_id);
         return 'queued';
     }
 
@@ -51,14 +62,14 @@ class WhatsappService
         return WhatsappQueue::count();
     }
 
-    public function select_number(){
+    public function select_number($admin_id = null){
         //frist check null numbers
-        $null_use_count = WhatsappNumber::where('admin_id',auth('admins')->id())->where('is_active',true)->where('is_block',false)->whereNull('last_used')->first();
+        $null_use_count = WhatsappNumber::where('admin_id',$admin_id ?? auth('admins')->id())->where('is_active',true)->where('is_block',false)->whereNull('last_used')->first();
         if($null_use_count){
             return $null_use_count;
         }
         //get by last used
-        $last_used = WhatsappNumber::where('admin_id',auth('admins')->id())->where('is_active',true)->where('is_block',false)->orderBy('last_used','desc')->first();
+        $last_used = WhatsappNumber::where('admin_id',$admin_id ?? auth('admins')->id())->where('is_active',true)->where('is_block',false)->orderBy('last_used','desc')->first();
         if($last_used){
             return $last_used;
         }
@@ -66,23 +77,23 @@ class WhatsappService
 
     }
 
-    public function active_number()
+    public function active_number($admin_id = null)
     {
-        $null_use_count = WhatsappNumber::where('admin_id',auth('admins')->id())->where('is_active',true)->where('is_block',false)->whereNull('last_used')->first();
+        $null_use_count = WhatsappNumber::where('admin_id',$admin_id ?? auth('admins')->id())->where('is_active',true)->where('is_block',false)->whereNull('last_used')->first();
         if($null_use_count){
             return $null_use_count;
         }
-        $last_used = WhatsappNumber::where('admin_id',auth('admins')->id())->where('is_active',true)->where('is_block',false)->where('last_used', '<', now()->subMinutes(5))->orderBy('last_used','desc')->first();
+        $last_used = WhatsappNumber::where('admin_id',$admin_id ?? auth('admins')->id())->where('is_active',true)->where('is_block',false)->where('last_used', '<', now()->subMinutes(5))->orderBy('last_used','desc')->first();
         if($last_used){
             return $last_used;
         }
         return null;
     }
 
-    public function add_to_queue($message,$phone,$customer_id=null,$project_id=null)
+    public function add_to_queue($message,$phone,$customer_id=null,$project_id=null,$admin_id = null)
     {
         WhatsappQueue::create([
-            'admin_id' => auth('admins')->id(),
+            'admin_id' => $admin_id ?? auth('admins')->id(),
             'message' => $message,
             'phone' => $phone,
             'customer_id' => $customer_id,
@@ -91,11 +102,11 @@ class WhatsappService
 
     }
 
-    public function log_create($number_id,$message,$phone, $status, $customer_id=null,$project_id=null,$response=null)
+    public function log_create($number_id,$message,$phone, $status, $customer_id=null,$project_id=null,$response=null,$admin_id = null)
     {
         WhatsappLog::create(attributes: [
             'whatsapp_number_id' => $number_id,
-            'admin_id' => auth('admins')->id(),
+            'admin_id' => $admin_id ?? auth('admins')->id(),
             'message' => $message,
             'customer_id' => $customer_id,
             'project_id' => $project_id,
